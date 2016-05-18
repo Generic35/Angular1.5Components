e6b2390cc017b79c0ccb9cf94064a1f9a4c2b103
elamonta <elamonta@teksystems.com>
2016-05-18 17:03:53 -0400

    added named controller function, DI annotations, data access through $http, html table using ng-repeat

--- ps-movies/movie-list.component.js
+++ ps-movies/movie-list.component.js
@@ -3,16 +3,23 @@
     
     var module = angular.module("psMovies");
     
+    function fetchMovies($http){
+        return $http.get("movies.json")
+            .then(function(response){ return response.data; });
+    }
+    
+    function controller ($http){
+        var model = this;
+        this.movies = [];
+        
+        model.$onInit = function(){ 
+            fetchMovies($http).then(function(movies){ model.movies = movies; }); 
+        };   
+    }   
+    
     module.component("movieList", {
         templateUrl: "/ps-movies/movie-list.component.html",
         controllerAs: "model",
-        controller: function(){
-            var model = this;
-            model.message = "old message";
-            
-            model.changeMessage = function(){
-                model.message = "new message";
-            };
-        }   
+        controller: ["$http", controller]
     });
 })();
