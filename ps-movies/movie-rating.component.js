41821b08df6e810ebd47297d6661977d1fa92eec
elamonta <elamonta@teksystems.com>
2016-05-19 14:33:02 -0400

    used $onInit directive event hook to create an are of stars using an empty array tracked by $index, then used the $onChanged event hook to update the component's model each time one of the bindings has changed, which in return triggers a digest cycle and updates the markup

--- ps-movies/movie-rating.component.js
+++ ps-movies/movie-rating.component.js
@@ -6,7 +6,20 @@
             templateUrl: "/ps-movies/movie-rating.component.html",
             bindings: {
                 value: "<"    
-            } 
+            },
+            controllerAs: "model",
+            controller: function(){
+                var model = this;
+                
+                model.$onInit = function(){
+                    model.entries = new Array(model.value);
+                };
+                
+                model.$onChanges = function() {
+                    model.entries = new Array(model.value);
+                };                
+            }
         });
+        
 
 })();
