a4fa50de5b6baad891f0ddc65f25574b23a9bb55
elamonta <elamonta@teksystems.com>
2016-05-19 13:31:24 -0400

    added rating feating by using ng-click on a button, providing it an event handler defined as a function inside the component

--- ps-movies/movie-list.component.js
+++ ps-movies/movie-list.component.js
@@ -15,6 +15,18 @@
         model.$onInit = function(){ 
             fetchMovies($http).then(function(movies){ model.movies = movies; }); 
         };   
+        
+        model.upRating = function(movie){
+            if(movie.rating < 5){
+                movie.rating += 1;
+            }
+        };
+        
+        model.downRating = function(movie){
+            if(movie.rating > 1){
+                movie.rating -= 1;
+            }
+        };
     }   
     
     module.component("movieList", {
