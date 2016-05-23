82ca37b1ca4ffae6346f5125e7bc6ab8595ec3fb
Yulia <juliam.n7@gmail.com>
2016-05-23 00:50:40 -0400

    in order to allow for two types of different stars (full vs empty) the array is filled with the two css classes and the rating component pupulated with the corresponding information

--- ps-movies/movie-rating.component.js
+++ ps-movies/movie-rating.component.js
@@ -1,26 +1,38 @@
 (function(){
 "use strict";
 
-    angular.module("psMovies")
-        .component("movieRating", {
-            templateUrl: "/ps-movies/movie-rating.component.html",
-            bindings: {
-                value: "<"    
-            },
-            transclude: true,
-            controllerAs: "model",
-            controller: function(){
-                var model = this;
-                
-                model.$onInit = function(){
-                    model.entries = new Array(model.value);
-                };
-                
-                model.$onChanges = function() {
-                    model.entries = new Array(model.value);
-                };                
-            }
-        });
+    var module = angular.module("psMovies");
+    
+    function buildEntries(value, max){
+        var entries = [];
+        
+        for(var i = 1; i <= max; i++){
+            var icon = i <= value ? "glyphicon-star" : "glyphicon-star-empty";
+            entries.push(icon);
+        }
+        
+        return entries;
+    }
+    
+    module.component("movieRating", {
+        templateUrl: "/ps-movies/movie-rating.component.html",
+        bindings: {
+            value: "<",
+            max: "<"    
+        },
+        controllerAs: "model",
+        controller: function(){
+            var model = this;
+            
+            model.$onInit = function(){
+                model.entries = buildEntries(model.value, model.max);
+            };
+            
+            model.$onChanges = function() {
+                model.entries = buildEntries(model.value, model.max);
+            };                
+        }
+    });
         
 
 })();
