b7cc51520e663609d6deab621b88ba3fa25e84a8
elamonta <elamonta@teksystems.com>
2016-05-24 19:03:46 -0400

    added the accordion component, blank

---
+++ ps-movies/accordion.component.js
@@ -0,0 +1,13 @@
+(function(){
+    "use strict";
+    
+    var module = angular.module("psMovies");
+    
+    module.component("accordion", {
+        template: "accordion"
+    });
+    
+    module.component("accordionPanel", {
+        tempalte: "accordionPanel" 
+    });
+})();
