039aa7486ec3101ad01548bf8115eceda0c89a35
elamonta <elamonta@teksystems.com>
2016-05-25 19:14:43 -0400

    added attribute binding allowing the parent component to pass in literal text without angular treating it as an expression (interpolating)

--- ps-movies/accordion.component.js
+++ ps-movies/accordion.component.js
@@ -7,10 +7,17 @@
         transclude: true,
         template: "<div class='panel-group' ng-transclude></div>"
     });
-    
+                        
     module.component("accordionPanel", {
+        bindings: {
+            "heading": "@"
+        },
         transclude: true,
+        controllerAs: "model",
         template:   "<div class='panel panel-default'>" +
+                        "<div class='panel-heading'> " +
+                            "<h4 class='panel-title'>{{ model.heading }}</h4>" +
+                        "</div>" +
                         "<div class='panel-body' ng-transclude>" +
                         "</div>" +
                     "</div>"
