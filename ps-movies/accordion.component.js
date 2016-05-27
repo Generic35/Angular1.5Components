cdd2ea7cfaa119ba2ae751d59ecd339e9d0f6289
elamonta <elamonta@teksystems.com>
2016-05-26 20:34:24 -0400

    added accordion controller

--- ps-movies/accordion.component.js
+++ ps-movies/accordion.component.js
@@ -5,7 +5,30 @@
     
     module.component("accordion", {
         transclude: true,
-        template: "<div class='panel-group' ng-transclude></div>"
+        template: "<div class='panel-group' ng-transclude></div>",
+        controller: function(){
+            var model = this;
+            var panels = [];
+            
+            model.selectPanel = function(panel){
+                for(var i in panels) {
+                    if(panel == panels[i]) {
+                        panels[i].turnOn();
+                    }
+                    else
+                    {
+                        panels[i].turnOff();
+                    }
+                }  
+            };
+            
+            model.addPanel = function(panel){
+                panels.push(panel);
+                if(panels.length > 0){
+                    panels[0].turnOn();
+                }
+            };
+        }
     });
                         
     module.component("accordionPanel", {
